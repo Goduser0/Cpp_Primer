cc37180d71c0424de2ae539d5011857b17a65e3c
Alienware <Alienware@noel.com>
2023-08-29 01:02:28 +0800

    create main.cpp

---
+++ Cpp_Primer/chap1_1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void chap1_1()
+{
+	printf("Hello World! chap1_1");
+}
---
+++ Cpp_Primer/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	printf("Hello World! in main");
+
+	return 0;
+}
