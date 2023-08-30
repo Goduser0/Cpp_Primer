5a7c689281efc5a141670704f42467165917816e
Alienware <Alienware@noel.com>
2023-08-31 01:55:33 +0800

    1

--- Chap1/main.cpp
+++ Chap1/main.cpp
@@ -150,6 +150,30 @@ void basic_cin()
 
 void basic_if()
 {
+	int currVal = 0, val = 0;
+	if (std::cin >> currVal)
+	{
+		int cnt = 1;
+		while (std::cin >> val)
+		{
+			if (val == currVal)
+			{
+				++cnt;
+			}
+			else
+			{
+				std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+				currVal = val;
+				cnt = 1;
+			}
+		}
+
+		std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+	}
+}
+
+void basic_class_io()
+{
 
 }
 
