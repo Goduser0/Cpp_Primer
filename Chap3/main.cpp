dd23fc27e0360aa6db23f0ea9e82a63efe3f7357
surface <surface@noel.com>
2023-09-10 22:33:43 +0800

    1

--- Chap3/main.cpp
+++ Chap3/main.cpp
@@ -7,6 +7,8 @@ using std::endl;
 using std::string;
 using std::getline;
 
+#include <vector>
+using std::vector;
 
 void demo_io_string() 
 {
@@ -93,17 +95,317 @@ void demo_range_for()
 	cout << punct_cnt << " punctuation characters in " << a << endl;
 }
 
+void demo_toupper()
+{
+	string s("Hello World!!!");
+	for (auto& c : s) {
+		c = toupper(c);
+	}
+	cout << s << endl;
+}
+
+void demo_index()
+{
+	//下标
+	string s("hello world");
+	s[0] = toupper(s[0]);
+	s[6] = toupper(s[6]);
+	cout << s << endl;
+
+	//使用下标进行迭代
+	string a("hello world");
+	for (decltype(a.size()) index = 0; index != a.size() && !isspace(a[index]); ++index) {
+		s[index] = toupper(s[index]);
+	}
+	cout << s << endl;
+
+	const string hexdigits = "0123456789ABCDEF";
+
+	string result;
+	string::size_type n;
+	while (cin >> n) {
+		if (n < hexdigits.size()) {
+			result += hexdigits[n];
+		}
+	}
+	cout << "Your hex number is:" << result << endl;
+}
+
+void q_3_6()
+{
+	string a;
+	while (cin >> a) {
+		for (auto& index : a) {
+			index = 'x';
+		}
+		cout << a << endl;
+	}
+}
+
+void q_3_10()
+{
+	string input, result;
+	while (cin >> input) {
+		for (decltype(input.size()) i = 0; i != input.size(); ++i) {
+			if (!ispunct(input[i])) {
+				result += input[i];
+			}
+		}
+		cout << result << endl;
+	}
+}
+
+void q_3_11()
+{
+	const string s = "Keep out!";
+	for (auto& c : s) {
+	}
+}
+
+void q_3_14()
+{
+	int i;
+	vector<int> ivec;
+	while (cin >> i) {
+		ivec.push_back(i);
+		cout << ivec.size() << endl;
+	}
+}
+
+void q_3_15()
+{
+	string s;
+	vector<string> svec;
+	while (cin >> s) {
+		svec.push_back(s);
+		cout << svec.size() << endl;
+	}
+}
+
+void demo_vector_index()
+{
+	vector<unsigned> score(11, 0);
+	unsigned grade;
+	while (cin >> grade) {
+		++score[grade / 10];
+	}
+
+	for (auto i : score)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+void q_3_16()
+{
+	vector<int> v1;
+	vector<int> v2(10);
+	vector<int> v3(10, 42);
+	vector<int> v4{ 10 };
+	vector<int> v5{ 10, 42 };
+	vector<string> v6{ 10 };
+	vector<string> v7{ 10, "hi" };
+	
+	vector<int> test = v5;
+	//vector<string> test = v6;
+	if (!test.empty()) {
+		cout << test.size() << endl;
+		for (auto i : test) {
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+	else {
+		cout << "It's empty" << endl;
+	}
+}
+
+void q_3_17()
+{
+	vector<string> svec;
+	string input;
+	while (cin >> input) {
+		svec.push_back(input);
+	}
+	for (auto& i : svec) {
+		for (auto& j : i) {
+			j = toupper(j);
+		}
+	}
+	for (auto i : svec) {
+		cout << i << endl;
+	}
+}
+
+void q_3_20()
+{
+	vector<int> ivec;
+	int i;
+	while (cin >> i) {
+		ivec.push_back(i);
+	}
+
+	for (int j = 0; j != (ivec.size() - 1); ++j) {
+		cout << ivec[j] + ivec[j + 1] << " ";
+	}
+	cout << endl;
+
+	for (int j = 0; j != ((ivec.size() + 1) / 2); ++j) {
+		cout << ivec[j] + ivec[ivec.size() - 1 - j] << " ";
+	}
+	cout << endl;
+}
+
+void demo_iterator()
+{
+	string s("some string");
+	if (s.begin() != s.end()) {
+		auto it = s.begin();
+		*it = toupper(*it);
+	}
+	cout << s << endl;
+}
+
+void q_3_21()
+{
+	vector<int> ivec;
+	int i;
+	while (cin >> i) {
+		ivec.push_back(i);
+	}
+
+	for (auto it = ivec.begin(); (it + 1) != ivec.end(); ++it) {
+		cout << *it + *(it + 1) << " ";
+	}
+	cout << endl;
+
+	auto begin = ivec.begin();
+	auto end = ivec.end() - 1;
+
+	while (begin <= end) {
+		cout << *begin + *end << " ";
+		++begin;
+		--end;
+	}
+	cout << endl;
+}
+
+void q_3_22()
+{
+	vector<string> text{ "hello", "" , "world"};
+	for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) {
+		for (auto i = it->begin(); i != it->end();i++){
+			*i = toupper(*i);
+		}
+		cout << *it << endl;
+	}
+}
+
+void q_3_23()
+{
+	vector<int> ivec(10, 2);
+	for (auto i = ivec.begin(); i != ivec.end(); ++i) {
+		*i = (*i) * 2;
+	}
+
+	for (auto i : ivec) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+void demo_binary_search()
+{
+	vector<int> text{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int sought = 7;
+
+	auto beg = text.begin(), end = text.end();
+	auto mid = text.begin() + (end - beg) / 2;
+	while (mid != end && *mid != sought) {
+		if (sought < *mid) {
+			end = mid;
+		}
+		else {
+			beg = mid + 1;
+		}
+		mid = beg + (end - beg) / 2;
+	}
+	cout << *mid << endl;
+}
+
+void q_3_24()
+{
+	vector<int> ivec;
+	int i;
+	while (cin >> i) {
+		ivec.push_back(i);
+	}
+
+	for (auto it = ivec.begin(); (it + 1) != ivec.end(); ++it) {
+		cout << *it + *(it + 1) << " ";
+	}
+	cout << endl;
+
+	auto begin = ivec.begin();
+	auto end = ivec.end() - 1;
+
+	while (begin <= end) {
+		cout << *begin + *end << " ";
+		++begin;
+		--end;
+	}
+	cout << endl;
+}
+
+void q_3_25()
+{
+	vector<unsigned> score(11, 0);
+	unsigned grade;
+	while (cin >> grade) {
+		if (grade <= 100) {
+			++(*(score.begin() + grade / 10));
+		}
+	}
+
+	for (auto i : score)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
 
 // main
 int main() 
 {
-	//demo_io_string();
+
 	//q_3_2_1();
 	//q_3_2_2();
 	//q_3_4_1();
 	//_3_4_2();
 	//q_3_5(true);
-	demo_range_for();
+	//q_3_6();
+	//q_3_10();
+	//q_3_11();
+	//q_3_14();
+	//q_3_15();
+	//q_3_16();
+	//q_3_17();
+	//q_3_20();
+	//q_3_21();
+	//q_3_22();
+	//q_3_23();
+	//q_3_24();
+	q_3_25();
+
+	//demo_io_string();
+	//demo_range_for();
+	//demo_toupper();
+	//demo_index();
+	//demo_vector_index()
+	//demo_iterator();
+	//demo_binary_search();
 
 	return 1;
 }
